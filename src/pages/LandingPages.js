48b67fa3165d6eedc84f1e632dced09b8480f07a
Sohibul Khahfi <sohibulkhahfi140310@gmail.com>
2023-02-02 03:03:32 +0700

    first commit

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import modulname from 'elements/Button'
+
+export default class Landing Page extends Component{
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
